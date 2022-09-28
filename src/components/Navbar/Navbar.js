120a1df5f4093c6213d334c536abd63a62c567a2
rubayetseason <season.ss2001@gmail.com>
2022-09-28 21:31:50 +0600

    navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className='nav'>
+            <marquee direction="left"><h2>Welcome to The Muscle Bar</h2></marquee>
+        </div>
+    );
+};
+
+export default Navbar;
