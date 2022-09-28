3bf67156992c01e34f28255f5d306fd105e6ef4d
rubayetseason <season.ss2001@gmail.com>
2022-09-28 22:57:03 +0600

    items added

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Body.css'
 import logo from '../../logo.png'
+import Exercise from '../Exercise/Exercise';
 const Body = () => {
+    const [exercises, setExercises] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setExercises(data))
+    }, [])
     return (
         <div className='body-container'>
             <div>
                 <div className='logo-container'>
                     <img src={logo} className='logopic' />
                     <h2 className='logo'>The Muscle Bar</h2>
+                </div>
+                <div className='exercise-container'>
+                    {
+                        exercises.map(exercise => <Exercise
+                            key={exercise.id}
+                            exercise={exercise}
+                        ></Exercise>)
+                    }
                 </div>
             </div>
             <div><h2>Dashboard</h2></div>
---
+++ src/components/Exercise/Exercise.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Exercise.css'
+const Exercise = (props) => {
+    const {exercise} = props;
+    const {name, img, descp, time} = exercise;
+    return (
+        <div className='item'>
+            <img src={img} className="item-img" />
+            <h2 className='center'>{name}</h2>
+            <p>{descp.slice(0, 80)}...</p>
+            <h5 className='center'>Time Required: {time} seconds</h5>
+            <button className='center btn'>Add to list</button>
+        </div>
+    );
+};
+
+export default Exercise;
